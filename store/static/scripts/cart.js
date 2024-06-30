22b7062983be632ade4f16508c5bbfe33f01f7ff
felipemadeit <felipemadeit@gmail.com>
2024-06-29 19:07:33 -0500

    95% cart view

--- store/static/scripts/cart.js
+++ store/static/scripts/cart.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 let totalPriceElement = document.getElementById(`total-price-${itemId}`);
                 totalPriceElement.textContent = formatPrice(quantity * price);
                 updateCartTotal();
+                updateTotalItems(); // Actualiza la cantidad total de productos
             } else {
                 alert("Hubo un problema al actualizar la cantidad.");
             }
@@ -40,22 +41,34 @@ document.addEventListener("DOMContentLoaded", function () {
         let grandTotal = 0;
         cartItems.forEach(item => {
             let totalPriceText = item.querySelector('[id^="total-price-"]').textContent;
-            let totalPrice = totalPriceText.replace(/[^\d.-]/g, "");
-            grandTotal += totalPrice;
+            // Remove currency symbol and convert formatted number to a float
+            let totalPrice = parseFloat(totalPriceText.replace(/[^\d,-]/g, "").replace(/\./g, "").replace(/,/g, "."));
+            grandTotal += isNaN(totalPrice) ? 0 : totalPrice;
         });
         document.getElementById('grand-total').textContent = formatPrice(grandTotal);
+    }
+
+    function updateTotalItems() {
+        let cartItems = document.querySelectorAll('.cart-item');
+        let totalItems = 0;
+        cartItems.forEach(item => {
+            let quantityElement = item.querySelector('select.quantity-sec');
+            let quantity = parseInt(quantityElement.value);
+            totalItems += isNaN(quantity) ? 0 : quantity;
+        });
+        document.getElementById('total-items').textContent = `${totalItems} Products`;
     }
 
     function formatInitialPrices() {
         let priceElements = document.querySelectorAll('[id^="total-price-"]');
         priceElements.forEach(element => {
-            let priceText = element.textContent.replace(/[^\d.-]/g, ""); // Remove non-numeric characters
+            let priceText = element.textContent.replace(/[^\d,-]/g, "").replace(/\./g, "").replace(/,/g, ".");
             let price = parseFloat(priceText);
-            element.textContent = formatPrice(price);
+            element.textContent = formatPrice(isNaN(price) ? 0 : price);
         });
-        updateCartTotal(); // Update the grand total after formatting individual prices
+        updateCartTotal();
+        updateTotalItems(); // Actualiza la cantidad total de productos al iniciar
     }
 
-    // Format all prices when the DOM is fully loaded
     formatInitialPrices();
 });
