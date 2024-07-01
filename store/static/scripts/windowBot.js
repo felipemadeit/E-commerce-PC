3e762b1f46a7b46229f32c938778b5ab92ddfdf7
felipemadeit <felipemadeit@gmail.com>
2024-06-30 21:06:18 -0500

    New styles to the chat section

--- store/static/scripts/windowBot.js
+++ store/static/scripts/windowBot.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     //Button chat bot
     const btnBot = document.querySelector('.button-chat');
     const windowBot = document.querySelector('.windowBot');
+    const closeBtn = document.querySelector('.close-img');
 
     function unfoldWindow () {
         console.log("hola");
-        if(windowBot.style.display == 'none') {
+        if(windowBot.style.display == 'none' || windowBot.style.display === '') {
             windowBot.style.display = 'flex';
         } else {
             windowBot.style.display = 'none'
@@ -14,9 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
         
     };
 
+    function closeWindow () {
+        windowBot.style.display = 'none';
+    }
+
     btnBot.addEventListener('click', function () {
         unfoldWindow();
     });
+
+    closeBtn.addEventListener('click', function() {
+        closeWindow();
+    })
 
    
 })
